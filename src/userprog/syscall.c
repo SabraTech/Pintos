10bc7cde53d96faeb7eed39b942a964cd21600ad
aly_thabet <alythabet@alythabet-Aspire-V3-571G>
2016-12-11 22:00:00 +0200

    fixed argument passing, 29/76 tests passing

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,120 +1,143 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include "threads/vaddr.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler (struct intr_frame *);
 
-int pop (int **stack);
-
-void halt (void) NO_RETURN;
-void exit (int status) NO_RETURN;
-void exit_ (void **esp, uint32_t *eax);
-// pid_t exec (const char *file);
-int wait (pid_t);
-bool create (const char *file, unsigned initial_size);
-bool remove (const char *file);
-int open (const char *file);
-int filesize (int fd);
-int read (int fd, void *buffer, unsigned length);
-void read_ (void **esp, uint32_t *eax);
-int write (int fd, const void *buffer, unsigned length);
-void write_ (void **esp, uint32_t *eax);
-void seek (int fd, unsigned position);
-unsigned tell (int fd);
-void close (int fd);
+static void *user_to_kernel_vaddr (void *uaddr);
+static void halt (void) NO_RETURN;
+static void exit (int status) NO_RETURN;
+static pid_t exec (const char *file);
+static int wait (pid_t);
+static bool create (const char *file, unsigned initial_size);
+static bool remove (const char *file);
+static int open (const char *file);
+static int filesize (int fd);
+static int read (int fd, void *buffer, unsigned length);
+static int write (int fd, const void *buffer, unsigned length);
+static void seek (int fd, unsigned position);
+static unsigned tell (int fd);
+static void close (int fd);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-int
-pop (int **stack)
-{
-  int ret = *(int *) *stack;
-  *stack += sizeof (int *);
-  return ret;
-}
+#define SYSNUM(s) *(int*) user_to_kernel_vaddr (s)
+#define ARG0(s) *(int*) user_to_kernel_vaddr (s + 4)
+#define ARG1(s) *(int*) user_to_kernel_vaddr (s + 8)
+#define ARG2(s) *(int*) user_to_kernel_vaddr (s + 12)
 
 static void
-syscall_handler (struct intr_frame *f) 
+syscall_handler (struct intr_frame *f)
 {
-  int *stack = f->esp;
-  int syscall_num = *(int*)stack;
-  printf ("in system call! %d - esp: %x - stack: %x\n", syscall_num, f->esp, stack);
-  switch (syscall_num) {
-    case SYS_WRITE:
-      {
-        // write_ (&stack, &f->eax); break;
-        stack  += 4;
-  			int fd = *(int*)stack;
-  			stack  += 4;
-  			void * buffer = *(char **)stack;
-  			stack  += 4;
-  			unsigned size = *(unsigned*)stack;
-  			if(fd ==1 )
-  			  putbuf((char *)buffer, size);
-        f->eax = size;
-        
-   			break;
-      }
+  switch (SYSNUM(f->esp)) {
+    case SYS_HALT:
+      halt ();
+      break;
     case SYS_EXIT:
-      {
-        stack  += 4;
-        int status = *(int*)stack;
-        exit (status);
-        break;
-      }
+      exit (ARG0(f->esp));
+      break;
+    case SYS_EXEC:
+      f->eax = exec (ARG0(f->esp));
+      break;
+    case SYS_WAIT:
+      f->eax = wait (ARG0(f->esp));
+      break;
+    case SYS_CREATE:
+      f->eax = create (ARG0(f->esp), ARG1(f->esp));
+      break;
+    case SYS_REMOVE:
+      f->eax = remove (ARG0(f->esp));
+      break;
+    case SYS_OPEN:
+      f->eax = open (ARG0(f->esp));
+      break;
+    case SYS_FILESIZE:
+      f->eax = filesize (ARG0(f->esp));
+      break;
+    case SYS_READ:
+      f->eax = read (ARG0(f->esp), ARG1(f->esp), ARG2(f->esp));
+      break;
+    case SYS_WRITE:
+      f->eax = write (ARG0(f->esp), ARG1(f->esp), ARG2(f->esp));
+      break;
+    case SYS_SEEK:
+      seek (ARG0(f->esp), ARG1(f->esp));
+      break;
+    case SYS_TELL:
+      f->eax = tell (ARG0(f->esp));
+      break;
+    case SYS_CLOSE:
+      close (ARG0(f->esp));
+      break;
+    default:
+      exit (-1);
   }
-  printf ("out system call! %d - esp: %x - stack: %x\n", syscall_num, f->esp, stack);
 }
 
 void
-write_ (void **esp, uint32_t *eax)
+halt (void)
 {
-  int fd = pop (esp);
-  void *buffer = pop (esp);
-  unsigned length = pop (esp);
-  
-  *eax = write (fd, "b", 2);
+  shutdown_power_off();
 }
 
 void
-read_ (void **esp, uint32_t *eax)
-{
-  int fd = pop (esp);
-  void *buffer = *(char **) pop (esp);
-  unsigned length = pop (esp);
-  
-  *eax = read (fd, buffer, length);
-}
-
-
-void
-exit_ (void **esp, uint32_t *eax)
-{
-  
-}
-
-void 
 exit (int status)
 {
   struct thread *cur = thread_current();
-  // if (thread_alive(cur->parent))
-  //   {
-  //     cur->cp->status = status;
-  //   }
-  //printf ("%s: exit(%d)\n", cur->name, status);
+  printf ("%s: exit(%d)\n", cur->name, status);
   thread_exit();
 }
 
-int 
+pid_t
+exec (const char *file)
+{
+
+}
+
+int wait (pid_t pid)
+{
+
+}
+bool create (const char *file, unsigned initial_size)
+{
+
+}
+
+bool remove (const char *file)
+{
+
+}
+
+int open (const char *file)
+{
+
+}
+
+int filesize (int fd)
+{
+
+}
+
+
+int
+read (int fd, void *buffer, unsigned length)
+{
+  return length;
+}
+
+int
 write (int fd, const void *buffer, unsigned length)
 {
-  if (fd == 1) 
+  buffer = user_to_kernel_vaddr (buffer);
+  if (fd == 1)
     {
       putbuf (buffer, length);
       return length;
@@ -122,8 +145,33 @@ write (int fd, const void *buffer, unsigned length)
   return -1;
 }
 
-int
-read (int fd, void *buffer, unsigned length)
+void seek (int fd, unsigned position)
 {
-  return length;
+
+}
+
+unsigned tell (int fd)
+{
+
+}
+
+void close (int fd)
+{
+
+}
+
+/* return kernel virtual address pointing to the physical address pointed to by
+   uaddr, to be used in kernel code.
+   If uaddr has no mapping in pdir, exits
+*/
+static void *
+user_to_kernel_vaddr (void *uaddr)
+{
+  struct thread *t = thread_current ();
+  void *kaddr = NULL;
+  if(is_user_vaddr(uaddr))
+    kaddr = pagedir_get_page (t->pagedir, uaddr);
+  if (kaddr == NULL)
+    exit (-1);
+  return kaddr;
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+typedef int pid_t;
+
 void syscall_init (void);
 
 #endif /* userprog/syscall.h */
